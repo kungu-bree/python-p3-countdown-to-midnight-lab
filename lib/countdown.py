37260bdc53ba9803de36d4fe5c814e4e0b126f5b
Brenda <brendakungu04@gmail.com>
2024-03-12 13:44:45 +0300

    countdown using while loop

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,12 @@
-# your code goes here!
+i = 10
+def countdown(i):
+
+
+    while i > 0:
+        print(f'{i} SECOND(S)!')
+        
+        i -= 1
+
+    print("HAPPY NEW YEAR!")
+        
+countdown(10)
